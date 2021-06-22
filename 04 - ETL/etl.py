1b401d4e5b753203fc9440960d44f966cbe34273
Lucas Silva <ola.lucassilva@gmail.com>
2021-06-21 21:25:35 -0300

    Commit inicial

---
+++ 04 - ETL/etl.py
@@ -0,0 +1,36 @@
+import csv
+import env
+import psycopg2
+
+conn_string = f'host={env.PGHOST} port=5432 dbname={env.PGDATABASE} user={env.PGUSER} password={env.PGPASSWORD}'
+connection = psycopg2.connect(conn_string)
+cursor = connection.cursor()
+
+with open('02 - Dados contidos nas tabelas/Dados_Tabela_Controlesessao.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    line_count = 0
+    inserts = []
+    for row in csv_reader:
+      is_header = line_count == 0
+      line_count += 1
+      
+      if is_header:
+        continue
+
+      Id = row[0]
+      DataAcesso = row[1]
+      Hash = row[2]
+      DataExpiracao = row[3]
+      Cliente = row[4]
+      TipoAcesso = row[5]
+      LoginAtendente = row[6]
+      TipoSessao = row[7]
+      Origem = row[8]
+      
+      inserts.append(f"""
+        insert into controle_sessao (Id,DataAcesso,Hash,DataExpiracao,Cliente,TipoAcesso,LoginAtendente,TipoSessao,Origem)
+        values ({Id},'{DataAcesso}','{Hash}','{DataExpiracao}','{Cliente}',{TipoAcesso},'{LoginAtendente}',{TipoSessao},{Origem});
+      """)
+
+    cursor.execute(''.join(inserts))
+    connection.commit()
